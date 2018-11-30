feffe515c71c2df0355c8da5153169474f5d254d
PazuC <pforpazu@gmail.com>
2018-12-01 01:08:38 +0800

    adapter

--- app/src/main/java/com/example/pazu/billyinstagram/Myadapter.java
+++ app/src/main/java/com/example/pazu/billyinstagram/Myadapter.java
@@ -12,40 +12,41 @@ import java.util.List;
 
 public class Myadapter extends RecyclerView.Adapter<Myadapter.ViewHolder> {
 
-    private List<String> mData;
+    private List<GetFromServer.Data> data;
 
-    Myadapter(List<String> data){
-        mData = data;
-    }
-    class ViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
         private TextView title;
         private TextView description;
 
-        ViewHolder( View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
-            imageView = (ImageView)itemView.findViewById(R.id.imageView);
-            title = ( TextView)itemView.findViewById(R.id.title);
-            description = ( TextView)itemView.findViewById(R.id.description);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
         }
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_layout,viewGroup,false);
-        return new ViewHolder(view);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_layout, parent, false);
+
+        return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        GetFromServer.Data getFromServer = data.get(position);
+        //holder.imageView.setI()  glide doing something with url;
+        holder.title.setText(getFromServer.getTitle());
+        holder.description.setText(getFromServer.getDesc());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
+        return data.size();
 
+    }
 
 }
