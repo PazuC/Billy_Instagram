61ea598b53387dbbb0c4690013eafd424db87e56
Pazu <pforpazu@gmail.com>
2018-12-01 12:47:17 +0800

    not finish onBindViewHolder

--- app/src/main/java/com/example/pazu/billyinstagram/Myadapter.java
+++ app/src/main/java/com/example/pazu/billyinstagram/Myadapter.java
@@ -7,47 +7,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.List;
 
-public class Myadapter extends RecyclerView.Adapter<Myadapter.MyViewHolder> {
+public class Myadapter extends RecyclerView.Adapter<Myadapter.ViewHolder> {
 
-    private List<GetFromServer.Data> mData;
+    private List<GetFromServer.Data> data;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
         private TextView title;
         private TextView description;
 
-        public MyViewHolder( View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
-            imageView = (ImageView)itemView.findViewById(R.id.imageView);
-            title = ( TextView)itemView.findViewById(R.id.title);
-            description = ( TextView)itemView.findViewById(R.id.description);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            description = (TextView) itemView.findViewById(R.id.description);
         }
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_layout,viewGroup,false);
-        return new RecyclerView.ViewHolder(view);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_layout, parent, false);
+
+        return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        GetFromServer.Data getFromServer = data.get(position);
+        //holder.imageView.set...()  glide doing something with url;
+        holder.title.setText(getFromServer.getTitle());
+        holder.description.setText(getFromServer.getDesc());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
+        return data.size();
 
+    }
 
 }
