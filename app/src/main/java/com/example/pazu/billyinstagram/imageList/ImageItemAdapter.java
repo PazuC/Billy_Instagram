cc01a8beaed21855fa85f3be6c8912249a52f0c1
Pazu <pforpazu@gmail.com>
2018-12-15 19:00:29 +0800

    change the mvp pattern
    username error done
    
    password error done

--- app/src/main/java/com/example/pazu/billyinstagram/imageList/ImageItemAdapter.java
+++ app/src/main/java/com/example/pazu/billyinstagram/imageList/ImageItemAdapter.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
 import com.example.pazu.billyinstagram.R;
 import com.example.pazu.billyinstagram.model.image.ImageItemResponse;
